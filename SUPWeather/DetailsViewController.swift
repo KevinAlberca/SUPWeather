d28cc69871dd767c9bc99c98a08ed2162957b674
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-12-14 17:14:02 +0100

    Remove TableVC in DetailVC

--- SUPWeather/DetailsViewController.swift
+++ SUPWeather/DetailsViewController.swift
@@ -13,19 +13,11 @@ import AlamofireImage
 class DetailsViewController: UIViewController {
 	
 	@IBOutlet weak var weatherIV: UIImageView!
-	var dailyWeather: [DailyWeather]?
 	var weatherIcon: String?
-	let detailTableVC = DetailsTableViewController()
+//	let detailTableVC = DetailsTableViewController()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		RequestManager.sharedInstance.fetchWeatherForDay(onSuccess: { [weak self] (result) in
-			print("\(result)")
-			self?.dailyWeather = result
-		}) { (error) in
-			print("Error : \(error)")
-		}
 		
 		if let iconName = weatherIcon,
 			let iconUrl = URL(string: RootTableDataSource.getIconUrl(iconName: iconName)) {
